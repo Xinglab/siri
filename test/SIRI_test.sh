1eae29830fa44200c35007bb420bd0d22e0012af
zcpan <zcpan1016@gmail.com>
2019-10-14 15:30:05 -0400

    Update SIRI_test.sh

--- test/SIRI_test.sh
+++ test/SIRI_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../bin/SIRI --gtf test.gtf --bam test_R1.bam,test_R2.bam --anchor 8 --length 100 --lib first -o SIRI_Output
+../bin/SIRI --gtf test.gtf --bam test_R1.bam,test_R2.bam --anchor 8 --length 100 --lib first --read P -o SIRI_Output
